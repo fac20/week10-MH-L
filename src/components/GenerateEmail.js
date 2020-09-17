1b5d5073723f135ecc579fcc7aec60e135053bbb
LiCern <lisacernilogar@me.com>
2020-09-17 12:24:39 +0100

    Create GenerateEmail and link to UserSelection
    Fixes #5
    
    Relates #3
    
    Co-authored-by: My Hoa Tien <myhoatien@gmail.com>

---
+++ src/components/GenerateEmail.js
@@ -0,0 +1,27 @@
+import React from "react";
+import userArray from "./../utils/users.js";
+
+const GenerateEmail = ({ username, murderee }) => {
+  const getUserInfo = userArray.filter((user) => user.github === username);
+
+  return (
+    <article>
+      <h2>Subject: HELP!!!</h2>
+      <h3>To: {getUserInfo[0].name}</h3>
+      <h3>From: Anonymous</h3>
+      <p>
+        {murderee} is about to be murdered! Only you can save them if you can
+        investigate the location, the weapon and the mudered to prevent this
+        atrocious heinous crime!
+        <br />
+        Hurry and do not waste any time, if you guess incorrectly the time bar
+        will decrease noticeably and {murderee}'s chances of survival will be
+        getting slimmer...
+        <br />
+        Lots of love, a fellow coder x
+      </p>
+      <button>What the FAC? Let's Solve some Crime!</button>
+    </article>
+  );
+};
+export default GenerateEmail;
