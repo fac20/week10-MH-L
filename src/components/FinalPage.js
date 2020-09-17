db30fd3b66bdd2a44f149ca702380176e6ff69a1
LiCern <lisacernilogar@me.com>
2020-09-17 22:33:30 +0100

    Fix timer bugs, create start again button
    Fixes #6 #1 #8 #9
    
    Co-authored-by: My Hoa Tien <myhoatien@gmail.com>

--- src/components/FinalPage.js
+++ src/components/FinalPage.js
@@ -1,32 +1,38 @@
 import React from "react";
 import getUserData from "./../utils/github_users.js";
 
-const Loser = ({ murderee, pageStatus, updatePageStaus }) => {
+const Loser = ({ murderee, pageStatus, updatePageStatus }) => {
   const [image, setImage] = React.useState([]);
   React.useEffect(() => {
     getUserData(murderee).then((user) => setImage(user.avatar_url));
   }, [murderee]);
-
+  const startAgain = () => {
+    window.location.reload();
+    updatePageStatus("home");
+  };
   return (
     <div>
       <h2>You let them die! Try again, better luck next time!</h2>
       <img src={image} alt="murdered person"></img>
-      <button>Redeem Yourself</button>
+      <button onClick={startAgain}>Redeem Yourself</button>
     </div>
   );
 };
 
-const Winner = ({ username, pageStatus, updatePageStaus }) => {
+const Winner = ({ username, pageStatus, updatePageStatus }) => {
   const [image, setImage] = React.useState([]);
   React.useEffect(() => {
     getUserData(username).then((user) => setImage(user.avatar_url));
   }, [username]);
-
+  const startAgain = () => {
+    window.location.reload();
+    updatePageStatus("home");
+  };
   return (
     <div>
       <h2>You're a winner!</h2>
       <img src={image} alt="Winner face"></img>
-      <button>Save another Life</button>
+      <button onClick={startAgain}>Save another Life</button>
     </div>
   );
 };
